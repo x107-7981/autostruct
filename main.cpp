e2d3e348a059ffe16be8acfcd3740af3f4c2a716
x107-7981 <x107_7981@163.com>
2024-11-24 14:35:20 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,23 @@ int main(){
     auto& [a,b,c,d] = x;
     c = true;
     scanf("%d%c",&a,&b);
+    int n;
+    scanf("%d",&n);
+    while(n--){
+        int k;
+        scanf("%d",&k);
+        d.push_back(k);
+    }
     cout << get<0>(x) << endl;
     putchar(x.getmember<2>());
     printf("\n%d\n",get<2>(x));
     printf("////////////////////////////\n");
-    x = make_autostruct(1,'0',false,d+d);
+    x = make_autostruct(1,'0',false,d);
     cout << get<0>(x) << endl;
     putchar(x.getmember<2>());
     printf("\n%d\n",get<2>(x));
+    for(auto i:x.getmember<4>())
+        cout << i << ' ';
 #else
     autounion<int,char,vector<int>,set<int>> b;
     b.getmember<1>() = 1234567;
